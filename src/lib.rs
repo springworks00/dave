bbcbbea15d42c03a920a209911115c9a9893400b
Dylan Jessen <djessen@calpoly.edu>
2023-11-27 19:06:04 -0700

    ready to test

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,14 @@
-use anyhow::Result;
 use std::net::*; 
-use std::thread;
+use std::{str, thread};
+use std::time::Duration;
 use std::collections::HashSet;
+
+use anyhow::Result;
 use xxhash_rust::xxh32::xxh32;
 
+const LOOPBACK: IpAddr = IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1));
+const MAX_PORT: u32 = 65536;
+const MIN_PORT: u32 = 49152;
 
 // do not connect() to this addr, because though you should
 // send to it, you will never be receiving from it.
@@ -21,8 +26,6 @@ pub fn group(msg: &str) -> Result<SocketAddr> {
     Ok(addr)
 }
 
-// somehow add yourself to the member set on the
-// local message forwarding service.
 pub fn join(new_member_sock: &UdpSocket, group: SocketAddr) -> Result<()> {
     // ping_sock must be different from new_member_sock bc:
     // - nms is not bound to 127.0.0.1
@@ -32,69 +35,72 @@ pub fn join(new_member_sock: &UdpSocket, group: SocketAddr) -> Result<()> {
     //   too inform the local group of its port
     // - the ping must only inform the local group to avoid
     //   multiple remote groups forwarding to the same addr
-    //   (resulting in duplicate packets)
+    //   (resulting in duplicate packets being received)
 
     let ping_sock = UdpSocket::bind("127.0.0.1:0")?;
 
     let local_group = format!("127.0.0.1:{}", group.port());
-    let local_member = format!("127.0.0.1:{}", new_member_sock.local_addr()?.port());
-    ping_sock.send_to(local_member.as_bytes(), local_group).unwrap();
+    let localized_member = format!("127.0.0.1:{}", new_member_sock.local_addr()?.port());
 
-    // ...
-    todo!();
-    // ping the universal multicast ip + the port of the
-    // global forwarding address.
-    // ip: 225.<WellHelloThereOldSport>
-    // port: group.ip
-    //
-    // this will 
+    ping_sock.send_to(localized_member.as_bytes(), local_group)?;
+
+    Ok(())
 }
 
-const LOOPBACK: IpAddr = IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1));
 
 fn local_forwarding_service(entrance: UdpSocket) {
-    let members: HashSet<SocketAddr> = HashSet::new();
+    let mut members: HashSet<SocketAddr> = HashSet::new();
     let exit = UdpSocket::bind("0.0.0.0:0").unwrap();
 
     let mut buf = [0; 1024];
     loop {
-        let (num_bytes, addr) = entrance.recv_from(&mut buf).unwrap();
-        if addr.ip() == LOOPBACK {
-            members.insert(&buf[..num_bytes]);
+        let Ok((num_bytes, addr)) = entrance.recv_from(&mut buf) else {
+            // blocking receipt failed
+            thread::sleep(Duration::from_millis(100));
+            continue;
+        };
+        if addr.ip() != LOOPBACK {
+            // addr is not a new member, so forward the data
+            for member in members.iter() {
+                let _ = exit.send_to(&buf[..num_bytes], member);
+            }
             continue;
         }
-        for member in members.iter() {
-            exit.send_to(&buf[..num_bytes], member).unwrap();
-        }
+        let Ok(addr) = str::from_utf8(&buf[..num_bytes]) else {
+            // new member addr is not utf8 data
+            continue;
+        };
+        let Ok(addr) = addr.parse::<SocketAddr>() else {
+            // new member addr is not a valid SocketAddr
+            continue;
+        };
+        members.insert(addr);
     }
 }
 
-fn hash_msg(msg: &str) -> (Ipv4Addr, u16) {
+fn hash_msg(msg: &str) -> (Ipv4Addr, u32) {
     let hash = xxh32(msg.as_bytes(), 0);
-
     let addr = Ipv4Addr::new(
         224,
         ((hash >> 13) & 0xFF) as u8,
         ((hash >> 21) & 0xFF) as u8,
         ((hash >> 29) & 0xFF) as u8,
     );
-    let port = 9999;
-    // XXX generate port from hash 
+    let port = hash % (MAX_PORT - MIN_PORT) + MIN_PORT;
     
     (addr, port)
 }
-
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn hash_msg() {
+    fn hash_msg_basic() {
         let phrase = "Well Hello There, Old Sport!";
         assert_eq!(
             hash_msg(phrase),
-            (Ipv4Addr::new(0, 0, 0, 0), 0),
+            (Ipv4Addr::new(224, 200, 82, 1), 52441),
         );
     }
 }
