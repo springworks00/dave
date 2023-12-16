52c934cd4be8ada276a1af5b75b71c898d858b37
Dylan Jessen <djessen@calpoly.edu>
2023-12-16 11:19:11 -0700

    please

--- sound.py
+++ sound.py
@@ -4,78 +4,124 @@ import pygame
 import pygame._sdl2.audio as sdl2_audio
 import subprocess
 from typing import *
+from subprocess import Popen
+import time
 
+import dave
+import sys
 
-
-def get_devices(capture_devices: bool = False) -> Tuple[str, ...]:
+def get_devices() -> Tuple[str, ...]:
     init_by_me = not pygame.mixer.get_init()
     if init_by_me:
         pygame.mixer.init()
-    devices = tuple(sdl2_audio.get_audio_device_names(capture_devices))
+    devices = tuple(sdl2_audio.get_audio_device_names(False))
     if init_by_me:
         pygame.mixer.quit()
+        #^this is necessary for some reason
     return devices
 
+def load_music(device_name: str, file_path: str, volume=1):
+    pygame.mixer.music.stop()
+    pygame.mixer.quit()
 
-def play(file_path: str, device: Optional[str] = None, volume=1):
-    if device is None:
-        devices = get_devices()
-        if not devices:
-            raise RuntimeError("No device!")
-        device = devices[0]
-    print("Play: {}\r\nDevice: {}".format(file_path, device))
-    pygame.mixer.init(devicename=device)
-    pygame.mixer.music.load(file_path)
-    pygame.mixer.music.set_volume(volume)
-    pygame.mixer.music.play()
-    #while pygame.mixer.music.get_busy():
-    #    time.sleep(0.1)
-    #pygame.mixer.quit()
-
-def load_music(device_name, file_path, volume=1):
     pygame.mixer.init(devicename=device_name)
+
     pygame.mixer.music.load(file_path)
     pygame.mixer.music.set_volume(volume)
 
-def play_music():#file_path: str), device: str, volume=1):
+def play_music():
     pygame.mixer.music.play()
 
 # message, sound, volume
 clones = [
-    #["#phone1", "resources/phone_ring.mp3", 1],
-    #["#phone1", "resources/phone_ring.mp3", 1],
-    #["#phone1", "resources/phone_ring.mp3", 1],
-    #["#phone1", "resources/phone_ring.mp3", 1],
-    #["#phone2", "resources/phone_ring.mp3", 1],
-    #["#phone3", "resources/phone_ring.mp3", 1],
-    ["#music1", "resources/music_box.mp3", 1],
-    ["#music1", "resources/music_box.mp3", 1],
-    ["#music1", "resources/music_box.mp3", 1],
-    ["#music1", "resources/music_box.mp3", 1],
-    ["#music1", "resources/music_box.mp3", 1],
-    ["#music1", "resources/music_box.mp3", 1],
     ["#music1", "resources/music_box.mp3", 1],
 ]
 
-nth_clone = 0
-#else:
-#    nth_clone = int(sys.argv[1])
-#
-#if nth_clone == len(clones):
-#    sys.exit()
-#
-#subprocess.Popen(f"python3 {__file__} {nth_clone+1}", shell=True)
+# if no arguments passed, this is the root script.
+# for every device found,
+#   spawn three scripts each listening for a different msg:
+#       %track1
+#       %track2
+#       %track3
+
+#nth_clone = 0
 #
 #device = nth_clone % len(get_devices())
-#print(f"CLONE #{nth_clone}: {get_devices()[device]}")
+#
+#msg, sound, volume = clones[nth_clone]
+#load_music(get_devices()[device], sound, volume)
 
-msg, sound, volume = clones[nth_clone]
-init_fast(get_devices()[device], sound, volume)
+def repl():
+    procs = []
+    devices = []
+    for d in get_devices():
+        if "Meta" in d or "Teams" in d:
+            continue
+        devices.append(d)
 
-while True:
-    _ = rx.receive(msg, block=True)
-    play_fast()
-    print("played sound fast")
-    #play(sound, get_devices()[device], volume=volume)
+    tracks = ["%track1", "%track2", "%track3"]
+    for d in devices:
+        for t in tracks:
+            procs.append(Popen([f"python3 {__file__} '{d}' '{t}'"], shell=True))
+
+    mt = dave.MemberTable()
+    _ = [mt.preload(t) for t in tracks]
+
+    time.sleep(1)
+    while True:
+        cmd = input("> ").strip().split(' ')
+        if cmd == "STOP":
+            mt.send(track, data=f"{resource} {volume}")
+            continue
+        if cmd == "TERMINATE":
+            [p.terminate() for p in procs]
+            break
+
+        if len(cmd) < 3:
+            print("not enough args (need 3)")
+            continue
+        track = cmd[0]
+        if int(track) > 3 or int(track) < 1:
+            print("invalid track: {track}")
+            continue
+        track = f"%track{track}"
+        resource = f"resources/{cmd[1]}.mp3"
+        if not os.path.exists(resource):
+            print(f"resource does not exist: {resource}")
+
+        volume = cmd[2]
+
+        print(f"sending: {track} {resource}")
+        mt.send(track, data=f"{resource} {volume}")
+        time.sleep(0.5)
+
+def child(device, track):
+    print(f"SCRIPT SPAWNED: {device} w/ {track}")
+    pygame.mixer.init()
+    mt = dave.MemberTable()
+
+    while True:
+        sound_path = mt.recv(track)
+        print(f"received path: {sound_path}")
+        if sound_path == "STOP":
+            break
+        sound_path, volume = sound_path.split(' ')
+
+        load_music(device, sound_path, float(volume))
+        play_music()
+
+    
+
+if len(sys.argv) == 1:
+    repl() #root script
+else:
+    child(sys.argv[1], sys.argv[2]) #child script
+
+
+
+#while True:
+#    play_music()
+#    print("played sound fast")
+#    #play(sound, get_devices()[device], volume=volume)
 
 
